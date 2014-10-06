44337778f9439311f29b61bf6223072c1e77b051
Joey Payne <jyapayne@gmail.com>
2014-10-07 06:03:04 +1300

    Added build file for mac.

---
+++ build_mac.bash
@@ -0,0 +1,21 @@
+rm -rf freeze
+
+cxfreeze main.py --target-dir=freeze --include-modules=PySide.QtGui,PySide,PySide.QtNetwork,PySide.QtCore
+chmod 755 freeze/*
+cp -rf freeze/* Web2Executable.app/Contents/MacOS/
+
+install_name_tool -change /usr/local/Cellar/pyside/1.2.1/lib/libpyside-python2.7.1.2.dylib @executable_path/libpyside-python2.7.1.2.dylib Web2Executable.app/Contents/MacOS/PySide.QtCore.so
+install_name_tool -change /usr/local/lib/libshiboken-python2.7.1.2.1.dylib @executable_path/libshiboken-python2.7.1.2.1.dylib Web2Executable.app/Contents/MacOS/PySide.QtCore.so
+install_name_tool -change /usr/local/lib/QtCore.framework/Versions/4/QtCore @executable_path/QtCore Web2Executable.app/Contents/MacOS/PySide.QtCore.so
+
+install_name_tool -change /usr/local/Cellar/pyside/1.2.1/lib/libpyside-python2.7.1.2.dylib @executable_path/libpyside-python2.7.1.2.dylib Web2Executable.app/Contents/MacOS/PySide.QtGui.so
+install_name_tool -change /usr/local/lib/libshiboken-python2.7.1.2.1.dylib @executable_path/libshiboken-python2.7.1.2.1.dylib Web2Executable.app/Contents/MacOS/PySide.QtGui.so
+install_name_tool -change /usr/local/lib/QtCore.framework/Versions/4/QtCore @executable_path/QtCore Web2Executable.app/Contents/MacOS/PySide.QtGui.so
+install_name_tool -change /usr/local/lib/QtGui.framework/Versions/4/QtGui @executable_path/QtGui Web2Executable.app/Contents/MacOS/PySide.QtGui.so
+
+install_name_tool -change /usr/local/Cellar/pyside/1.2.1/lib/libpyside-python2.7.1.2.dylib @executable_path/libpyside-python2.7.1.2.dylib Web2Executable.app/Contents/MacOS/PySide.QtNetwork.so
+install_name_tool -change /usr/local/lib/libshiboken-python2.7.1.2.1.dylib @executable_path/libshiboken-python2.7.1.2.1.dylib Web2Executable.app/Contents/MacOS/PySide.QtNetwork.so
+install_name_tool -change /usr/local/lib/QtCore.framework/Versions/4/QtCore @executable_path/QtCore Web2Executable.app/Contents/MacOS/PySide.QtNetwork.so
+install_name_tool -change /usr/local/lib/QtNetwork.framework/Versions/4/QtNetwork @executable_path/QtNetwork Web2Executable.app/Contents/MacOS/PySide.QtNetwork.so
+
+
