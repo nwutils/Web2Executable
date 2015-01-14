9e9857c6f524d135687fb13cae2605c43712e0e6
Joey Payne <jyapayne@gmail.com>
2015-01-15 10:41:49 +1300

    Added new build script for windows and icon.

--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,8 @@
 rd /S /Q Web2ExeWin
-call pyinstaller --onefile --hidden-import configobj --distpath command_line_builds -n web2exe-win command_line.py
+call pyinstaller --onefile --hidden-import configobj -i icon.ico --distpath command_line_builds -n web2exe-win command_line.py
 rd /S /Q command_line_builds\files
 echo D | xcopy /s files command_line_builds\files
 
-call cxfreeze.bat main.py --target-dir=Web2ExeWin --base-name=Win32GUI
+
+call pyinstaller -w --onefile --hidden-import configobj -i icon.ico --distpath Web2ExeWin -n Web2Exe main.py
 echo D | xcopy /s files Web2ExeWin\files
