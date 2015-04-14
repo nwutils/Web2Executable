1b87de20353690ab5db7449d726e5aac8f7c24e4
Joey Payne <jyapayne@gmail.com>
2015-04-13 22:42:19 -0600

    Added zip options to linux build.

--- build_command_line_linux.bash
+++ build_command_line_linux.bash
@@ -1,10 +1,13 @@
-rm -rf command_line_builds
+rm -rf Web2ExeLinux-CMD
 
-pyinstaller --onefile --hidden-import configobj --distpath command_line_builds -n web2exe-linux command_line.py
-cp -rf files command_line_builds/files
-rm -rf command_line_builds/files/downloads/*
+pyinstaller --onefile --hidden-import configobj --distpath Web2ExeLinux-CMD -n web2exe-linux command_line.py
+cp -rf files Web2ExeLinux-CMD/files
+rm -rf Web2ExeLinux-CMD/files/downloads/*
 
 rm -rf Web2ExeLinux
 pyinstaller -F --hidden-import configobj -n web2exe --distpath Web2ExeLinux main.py
 cp -rf files Web2ExeLinux/files
 rm -rf Web2ExeLinux/files/downloads/*
+
+zip -r -9 Web2ExeLinux-CMD.zip Web2ExeLinux-CMD/*
+zip -r -9 Web2ExeLinux-v0.2.0.zip Web2ExeLinux
