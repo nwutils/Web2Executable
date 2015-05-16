215f8f6b9f86e59e9429cea8883d0d447cbf67ec
Joey Payne <jyapayne@gmail.com>
2015-05-16 10:44:14 -0600

    Modified windows build script.

--- build_windows.bat
+++ build_windows.bat
@@ -6,3 +6,18 @@ echo D | xcopy /s files command_line_builds\files
 
 call pyinstaller -w --onefile --hidden-import PIL.Jpeg2KImagePlugin --hidden-import configobj -i icon.ico --distpath Web2ExeWin -n Web2Exe main.py
 echo D | xcopy /s files Web2ExeWin\files
+
+del Web2ExeWin\files\compressors\upx-mac
+del Web2ExeWin\files\compressors\upx-linux-x64
+del Web2ExeWin\files\compressors\upx-linux-x32
+
+del command_line_builds\files\compressors\upx-mac
+del command_line_builds\files\compressors\upx-linux-x64
+del command_line_builds\files\compressors\upx-linux-x32
+
+makensis /V4 Web2Exe.nsi
+
+7z a Web2ExeWin-v0.2.4b.zip -r Web2ExeWin
+cd command_line_builds
+7z a ..\Web2ExeWin-CMD.zip -tzip -r *
+cd ..
